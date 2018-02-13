fd205aae021b35b9e16a2304263f28d1b67ad5e6
dpereda <dpereda@gmail.com>
2018-02-13 13:59:15 -0500

    Finished Ep 14,  and just added react router dom

--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-    static propTypes ={
-      movie: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-      }),
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+
+const Movie = ({ movie }) => (
+  <div >
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+  </div>
+);
+
+export default Movie;
 
 
-    }
-
-    render() {
-      return (
-          <div >
-          <h3>{this.props.movie.title}</h3>
-
-        </div>
-      );
-    }
-}
-
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
